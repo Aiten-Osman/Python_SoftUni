16672e8581ee4f5b0ef56241b543c8ec9cca9b5f
Aiten-Osman <Aiten_06@abv.bg>
2026-09-21 22:58:51 +0300

    Add files via upload

---
+++ Python_Fundamentals/conditional_statements.py
@@ -0,0 +1,77 @@
+# name_person = input()
+# if name_person != "Johnny" :
+#     print (f"Hello, {name_person}!")
+# else:
+#     print ("Hello, my love!")
+
+
+# age_person = int(input())
+
+# if age_person <= 14:
+#     print("drink toddy")
+# elif age_person <= 18:
+#     print("drink coke")
+# elif age_person <= 21:
+#     print("drink beer")
+# else:
+#     print("drink whisky")
+
+
+# number_of_messages = int(input())
+# for _ in range(number_of_messages):
+#     number = int(input())
+#     if number == 88:
+#         print("Hello")
+#     elif number == 86:
+#         print("How are you?")
+#     elif number!= 88 and number != 86 and number < 88:
+#         print("GREAT!")
+#     elif number > 88:
+#         print("Bye.")
+
+
+# divisor = int(input())
+# boundary = int(input())
+# for i in range (boundary, 0, -1):
+#     if i % divisor == 0:
+#         if i <= boundary and i > 0:
+#             print(i)
+#         break
+
+
+# numbers_of_orders = int(input())
+# total_price = 0
+
+# for _ in range(numbers_of_orders):
+#     price_per_capsule = float(input())
+#     days = int(input())
+#     capsules_per_day = int(input())
+    
+#     if price_per_capsule < 0.01 or price_per_capsule > 100.00:
+#         continue
+#     if days < 1 or days > 31:
+#         continue
+#     if capsules_per_day < 1 or capsules_per_day > 2000:
+#         continue
+        
+#     current_order_price = price_per_capsule * capsules_per_day * days
+#     print(f"The price for the coffee is: ${current_order_price:.2f}")
+    
+#     total_price += current_order_price
+
+# print(f"Total: ${total_price:.2f}")
+
+
+# number_of_lines = int(input())
+# for _ in range(number_of_lines):
+#     line = input()
+#     if "," in line or "." in line or "_" in line:
+#         print(f"{line} is not pure!")
+#     else:
+#         print(f"{line} is pure.")
+
+
+# for _ in range(int(input())):
+#     if int(input()) != "End":
+        
+
